241bcefe3191e8c68fc68e1bf32668ee035e6078
Edvaldo José <edvaldojose003@gmail.com>
2026-04-04 04:54:22 -0300

    atualizacao das branchs

--- loop.bash
+++ loop.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for b in developer productiion; do
+for b in developer production; do
   echo "Atualizando as branch $b..."
   git checkout $b
   git merge main
 done
-
+echo "++++++++++++++"
 echo "[✔] Todas as branches foram atualizadas!..:)"
+echo "[✔] main > [✔] developer > [✔] production"
